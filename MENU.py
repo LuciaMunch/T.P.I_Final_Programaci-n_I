05d2ebe283ff2d5716a9705b8ad4ce20acb5612a
emiivillarruel <143727645+emiivillarruel@users.noreply.github.com>
2024-11-21 09:40:00 -0300

    Add files via upload

---
+++ MENU.py
@@ -0,0 +1,6 @@
+print("--------------")
+print("BIENVENIDOS 😊")
+print("--------------")
+print("1. AVENTURA MATEMÁTICA 🏝️🏴‍☠️ \n2. ADIVINA LA PELICULA A TRAVÉS DE EMOJIS 🎬\n3. PIEDRA 🗿, PAPEL 📃 O TIJERA ✂️\n4. AHORCADO 🔡\n5. ADIVINA LA PALABRA 💡\n6. PREGUNTAS Y RESPUESTAS ⁉️")
+
+juego = int(input("😄 Ingrese el número del juego que deseas jugar: "))
